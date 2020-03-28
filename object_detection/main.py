76e39e8d9f1b4e69e7fc808f928e14a37f0909dd
Ketan balbhim Kokane <ketankokane94@gmail.com>
2020-03-28 19:11:06 -0400

    added main.py

---
+++ object_detection/main.py
@@ -0,0 +1,26 @@
+# Author : Ketan Kokane <kk7471@rit.edu>
+from ObjectConfig import ObjectConfig
+from ObjectDataset import ObjectDataset
+from mrcnn.model import MaskRCNN
+
+
+def main():
+
+    config = ObjectConfig()
+    model = MaskRCNN(mode='training', model_dir='./', config=config)
+    model.load_weights('mrcnn/mask_rcnn_coco.h5', by_name=True,
+                       exclude=["mrcnn_class_logits", "mrcnn_bbox_fc",
+                                "mrcnn_bbox", "mrcnn_mask"])
+
+    train_set = ObjectDataset()
+    train_set.load_dataset('')
+    train_set.prepare()
+    print('Train: %d' % len(train_set.image_ids))
+    # train weights (output layers or 'heads')
+
+    model.train(train_set, train_set, learning_rate=config.LEARNING_RATE, epochs=5,
+                layers='heads')
+
+
+if __name__ == '__main__':
+    main()
