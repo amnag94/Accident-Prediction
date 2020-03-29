d096505da580abad5b809e2fff38e095ad704a16
Ketan balbhim Kokane <ketankokane94@gmail.com>
2020-03-29 14:43:27 -0400

    complete code to generate test set

--- object_detection/Dataset/GenerateDataset.py
+++ object_detection/Dataset/GenerateDataset.py
@@ -5,10 +5,25 @@ import shutil
 from Dataset.Constants import Annotation
 
 
+def merge_two_dictionary(car, not_car):
+    updates = 0
+    both = {}
+    for key in car:
+        if key in not_car:
+            not_car[key].extend(car[key])
+            both[key] = car[key] + not_car[key]
+            updates += 1
+    print(updates)
+    return both
+
+
 def parse_annotations():
     # have to parse all these files
-    dec = {}
-    objects = ['Car', 'Pedestrian']
+    car = {}
+    not_car = {}
+    car_count = 0
+    not_car_count = 0
+    objects = ['Car', 'Pedestrian', 'Bike', 'Truck']
     for subdir, dirs, files in os.walk('../../Annotations/'):
         for file in files:
             if file.endswith('.txt'):
@@ -24,11 +39,21 @@ def parse_annotations():
                             img = frame.split(':')[0]
                             box_cordinate =  eval(frame.split(':')[1].strip())
                             img = file.split('.')[0] + '/' + img + '.jpg'
-                            if img in dec:
-                                dec[img].append(Annotation(obj, box_cordinate))
+                            if obj == 'Car':
+                                add_to_dectionary(car, box_cordinate, img,
+                                                  'Car' )
                             else:
-                                dec[img] = [Annotation(obj, box_cordinate)]
-    return dec
+                                add_to_dectionary(not_car, box_cordinate, img,
+                                                  'Object But Not Car')
+
+    return car, not_car
+
+
+def add_to_dectionary(_dec, box_cordinate, img, obj):
+    if img in _dec:
+        _dec[img].append(Annotation(obj, box_cordinate))
+    else:
+        _dec[img] = [Annotation(obj, box_cordinate)]
 
 
 def write_annotations(data, file_name):
@@ -81,11 +106,28 @@ def make_dirs():
     make_image_folders('annotations/', 'test/')
 
 
+def generate_test_set(training_set, car, not_car):
+    training_set = {}
+
+
+    for dec in [car, not_car]:
+        idx = 1
+        for key in dec.keys():
+            if idx > 50:
+                break
+            if key not in training_set.keys():
+                training_set[key] = dec[key]
+                idx += 1
+    print(len(training_set))
+    return training_set
+
+
 if __name__ == '__main__':
     make_dirs()
-    dec = parse_annotations()
-    print(len(dec))
-    print(dec)
-    #
-    copy_files(dec, 'train')
-    copy_files(dec, 'test')
+    car, not_car = parse_annotations()
+    training_set = merge_two_dictionary(car,not_car)
+    print(len(training_set))
+    print(training_set)
+    testing_set = generate_test_set(training_set, car, not_car)
+    copy_files(training_set, 'train')
+    copy_files(testing_set, 'test')
