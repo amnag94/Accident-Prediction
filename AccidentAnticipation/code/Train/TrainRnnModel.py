b22213eef9abd977e4e92e3ada43db48da1768fb
Ketan balbhim Kokane <ketankokane94@gmail.com>
2020-04-16 19:11:51 -0400

    Moved the RNN model definition in RNN.py file
    Signed-off-by: Ketan balbhim Kokane <ketankokane94@gmail.com>

--- AccidentAnticipation/code/Train/TrainRnnModel.py
+++ AccidentAnticipation/code/Train/TrainRnnModel.py
@@ -10,25 +10,7 @@ from PIL import Image
 from Feature_Extraction.VGG_16 import MODEL_PATH
 
 
-class RNN(nn.Module):
-    def __init__(self, input_size, hidden_size):
-        super(RNN, self).__init__()
 
-        self.hidden_size = hidden_size
-
-        self.i2h = nn.Linear(input_size + hidden_size, hidden_size)
-        self.i2o = nn.Linear(input_size + hidden_size, 1)
-        self.sigmoid = nn.LogSoftmax(dim=1)
-
-    def forward(self, input, hidden):
-        combined = torch.cat((input, hidden), 1)
-        hidden = self.i2h(combined)
-        output = self.i2o(combined)
-        output = self.sigmoid(output)
-        return output, hidden
-
-    def initHidden(self):
-        return torch.zeros(1, self.hidden_size)
 
 
 n_hidden = 128
