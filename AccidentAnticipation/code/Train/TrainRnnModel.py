44a545057f7d2b0a4c217ed5feafb8f06dcb747d
Ketan balbhim Kokane <ketankokane94@gmail.com>
2020-04-19 00:36:40 -0400

    Completed Training Function

--- AccidentAnticipation/code/Train/TrainRnnModel.py
+++ AccidentAnticipation/code/Train/TrainRnnModel.py
@@ -1,19 +1,24 @@
 # Author : Ketan Kokane <kk7471@rit.edu>
 
 
+import math
 import os
 import random
 import time
+
 import numpy as np
-import math
 import torch.optim as optim
+
 import torch.nn as nn
 from Feature_Extraction.VGG_16 import *
 from Feature_Extraction.Generate_Features import *
 from Train.RNN import *
 
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+
 n_hidden = 128
-epochs = 3
+epochs = 5
 
 
 # TODO: add Exponential loss, and move it to CUDA
@@ -29,12 +34,14 @@ def _train(video_sequence_tensor, true_value_tensor, rnn, criterion, optimizer):
     hidden = rnn.initHidden()
     # rnn.zero_grad()
     optimizer.zero_grad()
-
+    prediction_list = []
     for i in range(video_sequence_tensor.size()[0]):
         # for ith frame in the video frame
         prediction_tensor, hidden = rnn(video_sequence_tensor[i], hidden)
+        prediction_list.append(prediction_tensor)
     # get prediction for every frame
-
+    prediction_tensor = torch.cat(prediction_list)
+    # true_value_tensor = true_value_tensor[:2]
     loss = criterion(prediction_tensor, true_value_tensor)
     # we want Exponential Loss here
     loss.backward()  # backpropogate
@@ -52,7 +59,7 @@ def train():
     rnn = RNN(4096, n_hidden)
 
     # TODO: Change this later to Exponential Loss
-    criterion = nn.NLLLoss()
+    criterion = nn.BCELoss()
 
     optimizer = optim.SGD(rnn.parameters(), lr=0.01, momentum=0.9)
     # Keep track of losses for plotting
@@ -65,8 +72,8 @@ def train():
 
         for data_item in dataset:
             video_clip = get_video_clip_from_training_set(data_item[0])
-            targets = get_targets(data_item[1])
-
+            targets = get_targets_tensor(data_item[1])
+            # TODO: make sure feature_tensors.device == cuda
             feature_tensors = get_features_tensors_for_video_clip(video_clip)
 
             loss = _train(feature_tensors, targets, rnn , criterion, optimizer)
@@ -74,12 +81,18 @@ def train():
             current_loss += loss
 
         all_losses.append(current_loss)
-        print('epochs', epochs, 'total NLL Loss in this epoch', current_loss,
-              'time since start', timeSince(start))
+        print('epochs=', epoch, 'total Loss in this epoch=', current_loss,
+              'time since start=', timeSince(start))
         current_loss = 0
         # Save the model
+        torch.save(rnn.state_dict(), '../../trained_models/rnn' + str(epoch) +
+                   '.model')
 
-    print(current_loss)# put this as an np array and store it in a file
+    # print(current_loss)# put this as an np array and store it in a file
+    plt.figure()
+    plt.plot(all_losses)
+    plt.show()
+    plt.savefig('total_loss.png')
 
 
 def timeSince(since):
@@ -100,20 +113,40 @@ def get_video_clip_from_training_set(video_clip_path):
     frames = []
     for filename in os.listdir(video_clip_path):
         # put some condition so only .jfg files are read
-        image = Image.open(video_clip_path + filename)
-        frames.append(image)
+        if filename.endswith('.jpg'):
+            image = Image.open(video_clip_path + filename)
+            frames.append(image)
     return frames
 
 
 def load_dataset():
-    lst = [('clip1', 'clip1targets.txt'),
-           ('clip2', 'clip2targets.txt'),
-           ('clip3', 'clip3targets.txt')]
+    #  TODO: Need to change this to load the actual dataset
+    lst = [('../../dataset/train/videoclips/clip_1/',
+            '../../dataset/train/groundtruth/clip_1.txt'),
+           ('../../dataset/train/videoclips/clip_2/',
+            '../../dataset/train/groundtruth/clip_2.txt')
+           ,('../../dataset/train/videoclips/clip_3/',
+            '../../dataset/train/groundtruth/clip_3.txt')
+        , ('../../dataset/train/videoclips/clip_4/',
+           '../../dataset/train/groundtruth/clip_4.txt')
+        , ('../../dataset/train/videoclips/clip_5/',
+           '../../dataset/train/groundtruth/clip_5.txt'),
+           ('../../dataset/train/videoclips/clip_6/',
+            '../../dataset/train/groundtruth/clip_6.txt')
+        , ('../../dataset/train/videoclips/clip_7/',
+           '../../dataset/train/groundtruth/clip_7.txt')
+        , ('../../dataset/train/videoclips/clip_8/',
+           '../../dataset/train/groundtruth/clip_8.txt')
+        , ('../../dataset/train/videoclips/clip_9/',
+           '../../dataset/train/groundtruth/clip_9.txt')
+        , ('../../dataset/train/videoclips/clip_10/',
+           '../../dataset/train/groundtruth/clip_10.txt')
+           ]
     return lst
 
 
-def get_targets(file_path):
-    return np.array([0, 0, 0, 0, 1, 1, 1])
+def get_targets_tensor(file_path):
+    return torch.from_numpy(np.loadtxt(file_path, dtype = np.float32) )
 
 
 if __name__ == '__main__':
